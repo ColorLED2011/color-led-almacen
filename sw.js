751ae50e8fe2bc3386b2760a91e8b4dba0e2cebf
ColorLED2011 <colorlednaguanagua@gmail.com>
2026-09-07 16:26:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
                Network-only para el Worker (API de Odoo/Telegram)
    ============================================================ */
 
-const CACHE = 'color-led-almacen-v8';
+const CACHE = 'color-led-almacen-v9';
 const ASSETS = [
   './index.html',
   './manifest.json',
